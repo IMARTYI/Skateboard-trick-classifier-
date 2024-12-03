69ddb1e8094e8819cb389ec8d5e846800af009e8
David <david2020martin@gmail.com>
2024-12-03 09:52:29 -0500

    Added video + Changed output writer parameters to match original video.

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import mediapipe as mp
 import numpy as np
 import os
 
-video_path = " "  # path to the videos of skateboarding tricks.
-output_video_path = " "  # Path to the output files of mp4s
+# Video is not loaded
+# Video is not loaded
+video_path = "./data/skate.mp4"  # path to the videos of skateboarding tricks.
+output_video_path = " .oout"  # Path to the output files of mp4s
 frame_rate = 60  # Frame rate for video
-resolution = (640, 480) # Resoltion for output video
+resolution = (640, 480)  # Resoltion for output video
+window_name = "Trick"
 
+cv2.namedWindow(window_name)
 # Load the input video
 cap = cv2.VideoCapture(video_path)
-# Save Recorded File
-out = cv2.VideoWriter("Outputvideo")
-# Video is not loaded
-if not cap.isOpened():
-    print("Error: video cannot be opened")
 
+if not cap.isOpened():  # Check if video can be loaded
+    print("Error: video cannot be opened")
     exit()
+
+original_fps = int(cap.get(cv2.CAP_PROP_FPS))
+original_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+original_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+output_resolution = (original_width, original_height)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = out = cv2.VideoWriter(output_video_path, fourcc, original_fps, output_resolution)
 
 while cap.isOpened():  # Capture each frame of the video
     ret, frame = cap.read()
